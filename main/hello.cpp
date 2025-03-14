0e94ccf482a7cddf3b29ecfe47cbbab62aaa0206
SupratikV <supratik.vishnu@gmail.com>
2025-03-14 09:21:41 +0530

    changes made

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
+  cout << "Made changes to hello.cpp file  \n";
+
   return 0;
 }
